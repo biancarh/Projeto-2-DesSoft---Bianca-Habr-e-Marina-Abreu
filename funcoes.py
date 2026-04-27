1ecbc6c2590bf09aac768a56ba33ca52a21fb530
Marina Vilela <marinaabreuvilela@gmail.com>
2026-04-27 13:24:17 -0300

    Questão 2

--- funcoes.py
+++ funcoes.py
@@ -5,3 +5,12 @@ def rolar_dados(n):
         num_sorteado= random.randint(1,6)
         lista_resposta.append(num_sorteado)
     return lista_resposta
+def guardar_dado(rolados, guardados, indice):
+    dado_a_ser_guardado= rolados[indice]
+    guardados.append(dado_a_ser_guardado)
+    saida_rolados=[]
+    for i in range (len(rolados)):
+        if i!=indice:
+            saida_rolados.append(rolados[i])
+    return [saida_rolados, guardados]
+
